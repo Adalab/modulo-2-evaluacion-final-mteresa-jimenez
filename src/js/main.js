c3445e3ac8d39543ac6a30327841fa5ff2030ac2
mteresa-jimenez <mteresa.jn@gmail.com>
2020-10-23 16:04:02 +0200

    Add search engine for series names (getData)

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,43 @@
 "use strict";
 
 let series = [];
-const seriesContainer = document.querySelector(".js-container");
 
-fetch(`http://api.tvmaze.com/search/shows?q=${name})`)
-  .then(function (response) {
-    console.log(response);
-    return response.json();
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    series = data.series;
-  });
-
-paintSearchEngine();
+function getData(ev) {
+  ev.preventDefault();
+  const inputElement = document.querySelector(".js-input-search");
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputElement.value}`)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      series = data;
+      paintSearchEngine();
+    });
+}
 
 // pintar HTML
 
 function paintSearchEngine() {
   let html = "";
-  html += "<form>";
-  html += '<label for="search">';
-  html +=
-    '<input type="text" id="search" placeholder="Nombre de la serie" class="js-input-search"/>';
-  html += '<button class="js-btn">Search</button>';
-  html += "</form>";
+
   html += "<ul>";
   for (const serie of series) {
+    console.log(serie.show.name);
     html += "<li>";
     html += "<div>";
-    html += `<img src=${series.image.medium}/>`;
-    html += `<h2>${serie.name}</h2>`;
+    html += `<img src=url(${serie.show.image.medium})/>`;
+    html += `<h2>${serie.show.name}</h2>`;
     html += "</div>";
     html += "</li>";
   }
   html += "</ul>";
-  seriesContainer.innerHTML = html;
+  const showsContainer = document.querySelector(".js-container");
+  showsContainer.innerHTML = html;
 }
+
+// listen search btn
+
+const btnElement = document.querySelector(".js-btn");
+btnElement.addEventListener("click", getData);
