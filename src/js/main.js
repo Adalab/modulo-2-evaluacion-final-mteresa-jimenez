6f8188e30af749547fef1ce641e5d494d5b2d44c
mteresa-jimenez <mteresa.jn@gmail.com>
2020-10-24 19:11:07 +0200

    Merge favorites branch into master

--- src/js/main.js
+++ src/js/main.js
@@ -25,7 +25,6 @@ function paintSearchEngine() {
 
   html += "<ul>";
   for (const serie of series) {
-    // console.log(serie);
     let classFavorite;
     const favoriteIndex = favorites.indexOf(serie.show.id);
     if (favoriteIndex === -1) {
@@ -54,25 +53,15 @@ function paintSearchEngine() {
 
 function markFavoriteSeries(ev) {
   const clicked = parseInt(ev.currentTarget.id);
-  // console.log("elemento clicado", clicked);
   const indexOfFavorite = favorites.indexOf(clicked);
-  // console.log("index", indexOfFavorite);
-  // const findFavorite= favorite.find(click=>{return click});
 
   if (indexOfFavorite === -1) {
-    console.log("lo añado");
     favorites.push(clicked);
-    // Creía que la opción más sencilla era esta, pero solamente selecciona el primer elemento de la lista listElement.classList.add("favorite").show.id;
   } else {
-    console.log("lo quito");
     favorites.splice(indexOfFavorite, 1);
-    // SPLICE: .splice(índice del elemento a partir del que quieres borrar, nº elementos que quieres borrar)
-    // listElement.classList.remove("favorite").show.id;
   }
   paintSearchEngine();
   listenFavoriteSeries();
-  // Cada vez que marcamos un favorito, es como si la página se volviese a cargar, por eso debemos pintar y escuchar otra vez
-  console.log("array de favoritos", favorites);
 }
 
 // listen favorite function
