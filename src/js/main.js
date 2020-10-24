692f3f90b0b8ce98703b2698fb9a08ea4b35c9d6
mteresa-jimenez <mteresa.jn@gmail.com>
2020-10-24 19:04:06 +0200

    Add favorite class to select or deselect favorite serie

--- src/js/main.js
+++ src/js/main.js
@@ -25,7 +25,15 @@ function paintSearchEngine() {
 
   html += "<ul>";
   for (const serie of series) {
-    html += `<li class="js-series-item" id="${serie.show.id}">`;
+    // console.log(serie);
+    let classFavorite;
+    const favoriteIndex = favorites.indexOf(serie.show.id);
+    if (favoriteIndex === -1) {
+      classFavorite = "";
+    } else {
+      classFavorite = "favorite";
+    }
+    html += `<li class="js-series-item ${classFavorite}" id="${serie.show.id}">`;
     html += '<div class="series-box">';
     if (serie.show.image === null) {
       html += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?
@@ -46,8 +54,25 @@ function paintSearchEngine() {
 
 function markFavoriteSeries(ev) {
   const clicked = parseInt(ev.currentTarget.id);
-  favorites.push(clicked);
-  console.log(favorites);
+  // console.log("elemento clicado", clicked);
+  const indexOfFavorite = favorites.indexOf(clicked);
+  // console.log("index", indexOfFavorite);
+  // const findFavorite= favorite.find(click=>{return click});
+
+  if (indexOfFavorite === -1) {
+    console.log("lo añado");
+    favorites.push(clicked);
+    // Creía que la opción más sencilla era esta, pero solamente selecciona el primer elemento de la lista listElement.classList.add("favorite").show.id;
+  } else {
+    console.log("lo quito");
+    favorites.splice(indexOfFavorite, 1);
+    // SPLICE: .splice(índice del elemento a partir del que quieres borrar, nº elementos que quieres borrar)
+    // listElement.classList.remove("favorite").show.id;
+  }
+  paintSearchEngine();
+  listenFavoriteSeries();
+  // Cada vez que marcamos un favorito, es como si la página se volviese a cargar, por eso debemos pintar y escuchar otra vez
+  console.log("array de favoritos", favorites);
 }
 
 // listen favorite function
