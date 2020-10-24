c5d4559e305e2592d09558a05149bd8a884a7a7f
mteresa-jimenez <mteresa.jn@gmail.com>
2020-10-24 16:39:45 +0200

    Create array with favorite series

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,20 @@
 "use strict";
 
 let series = [];
+let favorites = [];
 
 function getData(ev) {
   ev.preventDefault();
   const inputElement = document.querySelector(".js-input-search");
   fetch(`//api.tvmaze.com/search/shows?q=${inputElement.value}`)
     .then(function (response) {
-      console.log(response);
       return response.json();
     })
     .then((data) => {
       console.log(data);
       series = data;
       paintSearchEngine();
+      listenFavoriteSeries();
     });
 }
 
@@ -24,8 +25,7 @@ function paintSearchEngine() {
 
   html += "<ul>";
   for (const serie of series) {
-    console.log(serie.show.name);
-    html += "<li>";
+    html += `<li class="js-series-item" id="${serie.show.id}">`;
     html += '<div class="series-box">';
     if (serie.show.image === null) {
       html += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?
@@ -40,6 +40,23 @@ function paintSearchEngine() {
   html += "</ul>";
   const showsContainer = document.querySelector(".js-container");
   showsContainer.innerHTML = html;
+}
+
+// handle favorite function
+
+function markFavoriteSeries(ev) {
+  const clicked = parseInt(ev.currentTarget.id);
+  favorites.push(clicked);
+  console.log(favorites);
+}
+
+// listen favorite function
+
+function listenFavoriteSeries() {
+  const seriesItems = document.querySelectorAll(".js-series-item");
+  for (const seriesItem of seriesItems) {
+    seriesItem.addEventListener("click", markFavoriteSeries);
+  }
 }
 
 // listen search btn
