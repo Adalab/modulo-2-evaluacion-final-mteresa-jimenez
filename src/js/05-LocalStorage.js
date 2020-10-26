1b110504f77cebfd44f39c9381890c06d7026a5e
mteresa-jimenez <mteresa.jn@gmail.com>
2020-10-26 12:05:06 +0100

    Fix LocalStorage in master

--- src/js/05-LocalStorage.js
+++ src/js/05-LocalStorage.js
@@ -1,14 +1,17 @@
-"use strict";
-
 // localStorage
 function setLocalStorageFav() {
   localStorage.setItem("favorites", JSON.stringify(favorites));
 }
 
 function getLocalStorageFav() {
-  const LocalStorageFav = JSON.parse(localStorage.getItem(favorites));
-  favorites = LocalStorageFav;
+  const LocalStorageFav = JSON.parse(localStorage.getItem("favorites"));
+  if (LocalStorageFav == null) {
+    favorites = [];
+  } else {
+    favorites = LocalStorageFav;
+  }
   paintSearchEngine();
   listenFavoriteSeries();
   listenRemoveFavorite();
 }
+getLocalStorageFav();
